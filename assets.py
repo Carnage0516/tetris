841143ee9c2ecd3287a5badada7b69766714ae9b
Carnage0516 <124458115+Carnage0516@users.noreply.github.com>
2023-11-23 12:20:33 -0500

    Add files via upload

---
+++ assets.py
@@ -0,0 +1,167 @@
+import pygame
+import random
+from constants import (SCREEN_RESOLUTION,COLORS,)
+
+Iblock = [
+    [1,],
+    [1,],
+    [1,],
+    [1,]
+]
+Tblock = [
+    [0, 2, 0],
+    [2, 2, 2],
+]
+Oblock = [
+    [3, 3],
+    [3, 3],
+]
+Sblock = [
+    [4, 0],
+    [4, 4],
+    [0, 4],
+]
+ISblock = [
+    [0, 5],
+    [5, 5],
+    [5, 0],
+]
+
+Lblock = [
+    [6, 0],
+    [6, 0],
+    [6, 6],
+]
+ILblock = [
+    [0, 7],
+    [0, 7],
+    [7, 7],
+]
+
+block_list = [Iblock, Tblock, Oblock, Sblock, ISblock, Lblock, ILblock]
+
+
+class World():
+    """Class that defines the world in which the agents will live.
+    The world size is of 300x600 by default and contains 20 rows by 10 columns with a cell size of 30.
+    """
+
+    def __init__(self) -> None:
+        self.end = False
+        self.rows = 20
+        self.columns = 10
+        self.cell_size = 30
+        self.score = 0
+        self.size = (self.columns * self.cell_size, self.rows * self.cell_size)
+
+        self.grid = [[0 for _ in range(self.columns)] for _ in range(self.rows)]
+        self.grid[-1] = [1 for _ in range(self.columns)]
+        self.grid[-1][0] = 0
+
+        self.next_block = random.choice(block_list)
+        self.block = random.choice(block_list)
+        self.block_offset = [int(self.columns / 2) -1, 0]
+
+    def move(self, x, y) -> None:
+        self.block_offset[0] += x
+        if self.detect_collision():
+            self.block_offset[0] -= x
+
+        self.block_offset[1] += y
+        if self.detect_collision():
+            self.block_offset[1] -= y
+            if self.block_offset[1] <= len(self.block[1]):
+                self.end = True
+            self.fix_block()
+            self.clear_rows()
+
+            self.block = random.choice(block_list)
+            self.next_block = random.choice(block_list)
+            self.block_offset = [int(self.columns / 2) -1, 0]
+
+    def clear_rows(self) -> None:
+        for i, row in enumerate(self.grid):
+            if all(row):
+                self.grid.pop(i)
+                self.grid.insert(0, [0 for _ in range(self.columns)])
+                self.score += self.columns
+                print(self.score)
+
+    def fix_block(self) -> None:
+        for i, block_row in enumerate(self.block):
+            for j, block_element in enumerate(block_row):
+                if block_element != 0:
+                    self.grid[i + self.block_offset[1]][j + self.block_offset[0]] = block_element
+
+    def rotate(self) -> None:
+        before_state = self.block
+        self.block = list(zip(*self.block[::-1]))
+        if self.detect_collision():
+            self.block = before_state
+
+    def detect_collision(self) -> None:
+        #Detect end of screen
+        if self.block_offset[0] < 0:
+            return True
+        if self.block_offset[0] >= self.columns - len(self.block[0]) + 1:
+            return True
+        #Vertical collision
+        if self.block_offset[1] > self.rows - len(self.block):
+            return True
+        #Detect if there are block on the sides
+        for i, block_row in enumerate(self.block):
+            for j, block_element in enumerate(block_row):
+                if block_element != 0:
+                    if self.grid[i + self.block_offset[1]][j + self.block_offset[0]] != 0:
+                        return True
+
+    def draw(self, screen) -> None:
+        """draws the world in the middle of the screen"""
+        for i in range(self.rows):
+            for j in range(self.columns):
+                pos = (
+                    j * self.cell_size + SCREEN_RESOLUTION[0] / 2 - self.size[0] / 2,
+                    i * self.cell_size + SCREEN_RESOLUTION[1] / 2 - self.size[1] / 2,
+                    self.cell_size,
+                    self.cell_size,
+                )
+                pygame.draw.rect(
+                    screen,
+                    (COLORS[self.grid[i][j]]),
+                    pos,
+                    1 if self.grid[i] [j] == 0 else 0,
+                )
+
+        #Draw next block
+        for i, block_row in enumerate(self.next_block):
+            for j, block_element in enumerate(block_row):
+                pos = (
+                    j * self.cell_size + SCREEN_RESOLUTION[0] / 2 - self.size[0] / 2 + self.block_offset[0] * self.cell_size,
+                    i * self.cell_size + SCREEN_RESOLUTION[1] / 2 - self.size[1] / 2 + self.block_offset[1] * self.cell_size,
+                    self.cell_size,
+                    self.cell_size,
+                )
+                if block_element != 0:
+                    pygame.draw.rect(
+                        screen,
+                        COLORS[block_element],
+                        pos,
+                        0,
+                    )
+
+        #Draw current block
+        for i, block_row in enumerate(self.block):
+            for j, block_element in enumerate(block_row):
+                pos = (
+                    j * self.cell_size + SCREEN_RESOLUTION[0] / 2 - self.size[0] / 2 + self.block_offset[0] * self.cell_size,
+                    i * self.cell_size + SCREEN_RESOLUTION[1] / 2 - self.size[1] / 2 + self.block_offset[1] * self.cell_size,
+                    self.cell_size,
+                    self.cell_size,
+                )
+                if block_element != 0:
+                    pygame.draw.rect(
+                        screen,
+                        COLORS[block_element],
+                        pos,
+                        0,
+                    )
---
+++ tetris.py
@@ -0,0 +1,55 @@
+import pygame
+from constants import (
+    SCREEN_RESOLUTION,
+    FPS,
+    LIGHT_BLACK,
+    COOL_RED,
+)
+from assets import World
+
+def end_game():
+    pygame.quit()
+    quit()
+
+
+#Init
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_RESOLUTION)
+clock = pygame.time.Clock()
+
+grid = World
+terminate = False
+
+#Time event
+time_delay = 100
+timer_event = pygame.USEREVENT + 1
+pygame.time.set_timer(timer_event, time_delay)
+
+def game_loop_scene():
+    #Game loop
+    while not terminate:
+        clock.tick(FPS)
+        screen.fill(LIGHT_BLACK)
+
+        #Events
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    end_game()
+                if event.key == pygame.K_RIGHT:
+                    grid.move(1, 0)
+                if event.key == pygame.K_LEFT:
+                    grid.move(-1, 0)
+                if event.key == pygame.K_SPACE:
+                    grid.rotate()
+            elif event.type == timer_event:
+                grid.move(0, 1)
+        
+
+        #Draw
+        World()
+        grid()
+        grid.draw(screen)
+        pygame.display.update()
+
+game_loop_scene()
